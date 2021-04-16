cb70b867dd540e02678e9538510ef5df429bd598
Tobias Doninger <tobias.doninger@live.de>
2021-04-16 16:01:23 +0200

    Beispiel1

---
+++ node_modules/eslint-plugin-deprecation/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var rules = require("./rules");
+exports.rules = rules;
+//# sourceMappingURL=index.js.map
---
+++ node_modules/eslint-plugin-deprecation/dist/rules/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var deprecation_1 = require("./deprecation");
+exports.deprecation = deprecation_1.default;
+//# sourceMappingURL=index.js.map
